8edb6211b994dec100c664812c550730d8ed151f
usaghir <umer.saghir@live.com>
2023-06-25 15:39:18 +0200

    updated file names and dependencies

--- src/main/frontend/src/components/home/Home.js
+++ src/main/frontend/src/components/home/Home.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import SignUp from './SignUp';
 import Auth from '../../services/Auth';
-import NavBar from './NavBar';
+import LoginNavBar from './LoginNavBar';
 import Description from './Description';
 import '../Components.css';
+import Footer from './Footer';
 
 class Home extends Component {
   async register(registrationData) {
@@ -16,13 +17,14 @@ class Home extends Component {
   render() {
     return (
       <div className="">
-        <NavBar />
+        <LoginNavBar />
         <div className="container mt-5">
           <div className="grid-container mt-5">
             <Description text1="By Raja Umer" />
             <SignUp onSubmit={this.register} />
           </div>
         </div>
+        <Footer />
       </div>
     );
   }
