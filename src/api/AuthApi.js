d825cb05fd4523c5a69b84ab878f404f2d72f4a5
usaghir <umer.saghir@live.com>
2023-10-14 20:41:05 +0200

    update error handeling

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -2,11 +2,31 @@ import Api from "./Api";
 
 class AuthApi {
   authenticate({ email, password }) {
-    return Api.post("/authenticate", { email, password });
+    return Api.post("/authenticate", { email, password })
+      .then((response) => {
+        // Log the response data in the console
+       // console.log("Authentication response:", response.data);
+        return response; // Return the response to the caller
+      })
+      .catch((error) => {
+        // Handle any errors and log them
+        console.error("Authentication error:", error);
+        throw error; // Re-throw the error for further handling
+      });
   }
 
   register({ name, email, password }) {
-    return Api.post("/register", { name, email, password });
+    return Api.post("/register", { name, email, password })
+      .then((response) => {
+        // Log the response data in the console
+        //console.log("Registration response:", response.data);
+        return response; // Return the response to the caller
+      })
+      .catch((error) => {
+        // Handle any errors and log them
+        console.error("Registration error:", error);
+        throw error; // Re-throw the error for further handling
+      });
   }
 }
 
--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -14,12 +14,31 @@ class UserApi {
   };
   currentUserSet = () => {};
 
-  current() {
-    return Api.get("/user");
+  async current() {
+    try {
+      const response = await Api.get("/user");
+      // Log the response data in the console
+     // console.log("current user response:", response.data);
+      return response;
+    } catch (error) {
+      // Handle any errors and log them
+      console.error("current user error:", error);
+      throw error; // Re-throw the error for further handling
+    }
   }
 
   updateUser(user) {
-    return Api.put("/user", user);
+    return Api.put("/user", user)
+      .catch.then((response) => {
+        // Log the response data in the console
+        //console.log("update user response:", response.data);
+        return response; // Return the response to the caller
+      })
+      .catch((error) => {
+        // Handle any errors and log them
+        console.error("update user error:", error);
+        throw error; // Re-throw the error for further handling
+      });
   }
 
   async loadUser() {
