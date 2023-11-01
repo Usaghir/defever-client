a1de1ee817d6c2514ff5d8bd2abd400e07c08ef4
usaghir <umer.saghir@live.com>
2023-11-01 18:50:26 +0100

    change URL to GCP

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Auth from "../services/Auth";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://35.208.150.162:8080/";
 
 const Api = axios.create({
   baseURL: BASE_URL,
